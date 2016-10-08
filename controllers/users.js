effe2ac3912ea36891d5e113b1890477b7bec00a
austincunningham <austincunningham@hotmail.com>
2016-10-08 16:35:49 +0100

    changed to default route as /users/delete/:id not working

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,16 @@
+
+
 var User = require('../models/user');
 var express = require('express');
 var router = express.Router();
 
-
 // GET /users
 // Get a list of users
-router.get('/', function(req, res) {
-  User.find({}, function(err, users) {
+router.get('/', function (req, res) {
+  User.find({}, function (err, users) {
     if (err) {
       return res.status(500).json({
-        error: "Error listing users: " + err
+        error: 'Error listing users: ' + err,
       });
     }
 
@@ -19,13 +20,13 @@ router.get('/', function(req, res) {
 
 // GET /users/:id
 // Get a user by ID
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   User.findOne({
-    _id: req.params.id
-  }, function(err, user) {
+    _id: req.params.id,
+  }, function (err, user) {
     if (err) {
       return res.status(500).json({
-        error: "Error reading user: " + err
+        error: 'Error reading user: ' + err,
       });
     }
 
@@ -37,10 +38,9 @@ router.get('/:id', function(req, res) {
   });
 });
 
-
 // DELETE /users/delete/:id
 // Get an user by ID and DElETE
-router.delete('/delete/:id', function (req, res) {
+router.delete('/:id', function (req, res) {
   User.findOne({
     _id: req.params.id,
   }, function (error, user) {
@@ -57,6 +57,5 @@ router.delete('/delete/:id', function (req, res) {
 
   }).remove();
 });
-
 
 module.exports = router;
